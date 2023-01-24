222fee69252fd8fcf0651e4232822234dfe1be2b
karenis32 <karen.is32@hotmail.com>
2023-01-24 18:34:28 -0300

    commit-model-categoria

--- WebApiControlStock/Models/Categoria.cs
+++ WebApiControlStock/Models/Categoria.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApiControlStock.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiControlStock.Models
 {
+    [Table ("Categoria")]
     public class Categoria
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Nombre { get; set; }
     }
 }
