ecc7899912d7bfc92d2f4276a7f9e130cd845ece
karenis32 <karen.is32@hotmail.com>
2023-01-24 18:37:45 -0300

    commit-arreglo-models

--- WebApiControlStock/Models/Producto.cs
+++ WebApiControlStock/Models/Producto.cs
@@ -4,24 +4,26 @@ using System.Data.SqlTypes;
 
 namespace WebApiControlStock.Models
 {
-        [Table("Producto")]
-        public class Producto
-        {
-            [Key]
-            public int Id { get; set; }
+    [Table("Producto")]
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
 
-            [Required]
-            [Column(TypeName = "varchar(50)")]
-            public string Nombre { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Nombre { get; set; }
 
-            [Required]
-            public char LineaProducto { get; set; }
+        [Required]
+        [Column(TypeName = "char(1)")]
+        public string LineaProducto { get; set; }
 
-            public SqlMoney Precio { get; set; }
+        [Column(TypeName = "money")]
+        public double Precio { get; set; }
 
-            public int CategoriaId { get; set; }
-            [ForeignKey("CategoriaId")]
-            public Categoria Categoria { get; set; }
-        }
+        public int CategoriaId { get; set; }
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+    }
 }
 
