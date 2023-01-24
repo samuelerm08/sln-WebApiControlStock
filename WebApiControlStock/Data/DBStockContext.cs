bb59b7051e14c720d067d7f9f47853ba4738b62d
NicolasRamos <nicolasramos.borner@gmail.com>
2023-01-24 18:55:36 -0300

    DBStockContext

---
+++ WebApiControlStock/Data/DBStockContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiControlStock.Models;
+
+namespace WebApiControlStock.Data
+{
+    public class DBStockContext : DbContext
+    {
+        public DBStockContext(DbContextOptions<DBStockContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Producto> Productos { get; set; }
+
+        public virtual DbSet<Categoria> Categorias { get; set; }
+
+
+    }
+}
--- WebApiControlStock/Startup.cs
+++ WebApiControlStock/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,8 @@ namespace WebApiControlStock
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddNewtonsoftJson(o => o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
             services.AddControllers();
         }
 
